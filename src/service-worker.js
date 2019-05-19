bece393d82a653418810ae016d4a635e811322fa
eduhumb <eduhumb@gmail.com>
2019-05-19 20:59:48 -0300

    atualização service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,6 +5,89 @@
 importScripts('./scripts/libs/idb-keyval.js');
 importScripts('./scripts/analytics-sw.js');
 
+
+const version = "1.0.0";
+const cacheName = `pushExample-${version}`;
+
+const check = () => {
+  if (!('serviceWorker' in navigator)) {
+    throw new Error('No Service Worker support!')
+  }
+  if (!('PushManager' in window)) {
+    throw new Error('No Push API Support!')
+  }
+}
+
+const registerServiceWorker = async () => {
+  const swRegistration = await navigator.serviceWorker.register('service-worker.js')
+  return swRegistration
+}
+
+const requestNotificationPermission = async () => {
+  const permission = await window.Notification.requestPermission()
+  // value of permission can be 'granted', 'default', 'denied'
+  // granted: user has accepted the request
+  // default: user has dismissed the notification permission popup by clicking on x
+  // denied: user has denied the request.
+  if (permission !== 'granted') {
+    throw new Error('Permission not granted for Notification')
+  } else {
+    console.log('Permission granted')
+  }
+}
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/push.html`,
+        `/api1.html`,
+        `/api2.html`,
+        `/referencias.html`,
+        `/css/style_api.css`,
+        `/css/main.css`,
+        `/css/index.css`,
+        `/images/agateemieliD.gif`,
+        `/images/agateemieliE.gif`,
+        `/images/badge-72x72.png`,
+        `/images/criatividade-off.png`,
+        `/images/icon-css3.png`,
+        `/images/icon-html5.png`,
+        `/images/king.gif`,
+        `/images/logo-192x192.png`,
+        `/images/logo-32x32.png`,
+        `/images/logo-512x512.png`,
+        `/images/logo-72x72.png`,
+        `/images/logo.svg`,
+        `/images/placanomes.png`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+const main = async () => {
+  check()
+  const swRegistration = await registerServiceWorker()
+  const permission = await requestNotificationPermission()
+}
+
 self.analytics.trackingId = 'UA-77119321-2';
 
 self.addEventListener('push', function(event) {

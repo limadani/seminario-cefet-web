0b1cfca8108c77b3b8da1564756d3a9034aed1e0
Eduardo <eduhumb@gmail.com>
2019-05-21 07:09:38 -0300

    Comentadas as partes desnecessárias

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,36 +5,35 @@
 importScripts('./scripts/libs/idb-keyval.js');
 importScripts('./scripts/analytics-sw.js');
 
-
 const version = "1.0.0";
 const cacheName = `pushExample-${version}`;
 
-const check = () => {
-  if (!('serviceWorker' in navigator)) {
-    throw new Error('No Service Worker support!')
-  }
-  if (!('PushManager' in window)) {
-    throw new Error('No Push API Support!')
-  }
-}
+// const check = () => {
+//   if (!('serviceWorker' in navigator)) {
+//     throw new Error('No Service Worker support!')
+//   }
+//   if (!('PushManager' in window)) {
+//     throw new Error('No Push API Support!')
+//   }
+// }
 
-const registerServiceWorker = async () => {
-  const swRegistration = await navigator.serviceWorker.register('service-worker.js')
-  return swRegistration
-}
+// const registerServiceWorker = async () => {
+//   const swRegistration = await navigator.serviceWorker.register('service-worker.js')
+//   return swRegistration
+// }
 
-const requestNotificationPermission = async () => {
-  const permission = await window.Notification.requestPermission()
-  // value of permission can be 'granted', 'default', 'denied'
-  // granted: user has accepted the request
-  // default: user has dismissed the notification permission popup by clicking on x
-  // denied: user has denied the request.
-  if (permission !== 'granted') {
-    throw new Error('Permission not granted for Notification')
-  } else {
-    console.log('Permission granted')
-  }
-}
+// const requestNotificationPermission = async () => {
+//   const permission = await window.Notification.requestPermission()
+//   // value of permission can be 'granted', 'default', 'denied'
+//   // granted: user has accepted the request
+//   // default: user has dismissed the notification permission popup by clicking on x
+//   // denied: user has denied the request.
+//   if (permission !== 'granted') {
+//     throw new Error('Permission not granted for Notification')
+//   } else {
+//     console.log('Permission granted')
+//   }
+// }
 
 self.addEventListener('install', e => {
   e.waitUntil(
@@ -82,11 +81,11 @@ self.addEventListener('fetch', event => {
   );
 });
 
-const main = async () => {
-  check()
-  const swRegistration = await registerServiceWorker()
-  const permission = await requestNotificationPermission()
-}
+// const main = async () => {
+//   check()
+//   const swRegistration = await registerServiceWorker()
+//   const permission = await requestNotificationPermission()
+// }
 
 self.analytics.trackingId = 'UA-77119321-2';
 

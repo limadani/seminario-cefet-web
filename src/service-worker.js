60d691f7cd980562b8e01abc2982db9394278f17
DanielaLourenco <lourencodaniela.s@gmail.com>
2019-05-21 09:17:45 -0300

    Remocao de codigo desnecessario

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,33 +8,6 @@ importScripts('./scripts/analytics-sw.js');
 const version = "1.0.0";
 const cacheName = `pushExample-${version}`;
 
-// const check = () => {
-//   if (!('serviceWorker' in navigator)) {
-//     throw new Error('No Service Worker support!')
-//   }
-//   if (!('PushManager' in window)) {
-//     throw new Error('No Push API Support!')
-//   }
-// }
-
-// const registerServiceWorker = async () => {
-//   const swRegistration = await navigator.serviceWorker.register('service-worker.js')
-//   return swRegistration
-// }
-
-// const requestNotificationPermission = async () => {
-//   const permission = await window.Notification.requestPermission()
-//   // value of permission can be 'granted', 'default', 'denied'
-//   // granted: user has accepted the request
-//   // default: user has dismissed the notification permission popup by clicking on x
-//   // denied: user has denied the request.
-//   if (permission !== 'granted') {
-//     throw new Error('Permission not granted for Notification')
-//   } else {
-//     console.log('Permission granted')
-//   }
-// }
-
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
@@ -80,12 +53,6 @@ self.addEventListener('fetch', event => {
     })
   );
 });
-
-// const main = async () => {
-//   check()
-//   const swRegistration = await registerServiceWorker()
-//   const permission = await requestNotificationPermission()
-// }
 
 self.analytics.trackingId = 'UA-77119321-2';
 
